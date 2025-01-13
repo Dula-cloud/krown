ab8adbe32257eaf48ae74cdd372565db36043019
Dulangana Dhamsara <127759019+Dula-cloud@users.noreply.github.com>
2025-01-14 02:09:37 +0530

    changes

---
+++ header.php
@@ -0,0 +1,21 @@
+<header>
+    <div style="display: flex;">
+        <img class="logoNav" src="Resources/logo.png">
+    </div>
+    <div class="navFirst">
+        <ul class="navItemsMain">
+            <li><a class="navItemsInMain" href="#">Home</a></li>
+            <li><a class="navItemsInMain" href="#">Sale</a></li>
+            <li><a class="navItemsInMain" href="#">Blogs</a></li>
+            <li><a class="navItemsInMain" href="#">Contact</a></li>
+        </ul>
+    </div>
+    <div style="display: flex;">
+        <ul class="navItemsSub">
+            <li><a class="navItemsInSub" href="#">Log In</a></li>
+            <li><a class="navItemsInSub" href="#">Sign Up</a></li>
+        </ul>
+    </div>
+
+
+</header>
---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Krown</title>
+    <link rel="stylesheet" href="Styles/style.css"/>
+</head>
+<body>
+    <?php
+        require 'header.php';
+    ?>
+    
+
+
+</body>
+</html>
